20ac975598d7d4b0042287cffa779d44bfe19183
YuriyMikh <96769010+YuriyMikh@users.noreply.github.com>
2023-06-02 21:55:52 +0000

    Deploying to gh-pages from @ YuriyMikh/goit-js-hw-10-2@54efacb8e10c8075a77024d6a476d8f6205b2099 🚀

--- index.7c134123.js
+++
@@ -1,2 +0,0 @@
-!function(){var e="api_key=live_gMvLfc5Q1BBvM4bZ9764sbQq3tGdsdgN4rwcVPRHY1cX9R2iiV8TOUldepTICVtM";var t;document.querySelector(".breed-select"),document.querySelector(".cat-info");(t="beng",fetch("".concat("https://api.thecatapi.com/v1/images/search","?breed_ids=").concat(t,"&").concat(e)).then((function(e){if(!e.ok)throw new Error(e.status);return e.json()}))).then((function(e){return console.dir(e[0].breeds[0].temperament)}))}();
-//# sourceMappingURL=index.7c134123.js.map
--- index.ef95f5cd.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".breed-select"),document.querySelector(".cat-info");var e;(e="beng",fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${e}&api_key=live_gMvLfc5Q1BBvM4bZ9764sbQq3tGdsdgN4rwcVPRHY1cX9R2iiV8TOUldepTICVtM`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()}))).then((e=>console.dir(e[0].breeds[0].temperament)));
-//# sourceMappingURL=index.ef95f5cd.js.map
